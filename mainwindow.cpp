467392e9f476fb5dbe9038f113eed1a09eabe0bf
ZubinHathiram <pys13@txstate.edu>
2025-07-21 17:55:03 -0500

    Fixed bad initialization of gameLoop

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,17 +1,16 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-#include "gameloop.h"
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , gameLoop(this)
 {
     ui->setupUi(this);
 
-    GameLoop *gameLoop = new GameLoop(this);
-
-    connect(ui->pauseButton, SIGNAL(clicked()), gameLoop , SLOT(pause()));
-    connect(ui->playButton, SIGNAL(clicked()), gameLoop , SLOT(play()));
+    connect(ui->pauseButton, SIGNAL(clicked()), &gameLoop , SLOT(pause()));
+    connect(ui->playButton, SIGNAL(clicked()), &gameLoop , SLOT(play()));
 }
 
 MainWindow::~MainWindow()
