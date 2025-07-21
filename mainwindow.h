0cdbfdd6727a978e077a069c2fefc255e2a71d66
zipppzy <zipppzy939@gmail.com>
2025-07-21 17:55:03 -0500

    Fixed bad initialization of gameLoop

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "gameloop.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +18,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    GameLoop gameLoop;
 };
 #endif // MAINWINDOW_H
